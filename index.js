c8f904410f7081669ae43ce8cfa35f2bcd68d162
github username pianistps <pianistps@yahoo.com>
2018-01-17 12:43:20 -0600

    passing the recipe form test and recipe template test

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 function init() {
   //put any page initialization/handlebars initialization here
+
+  // Register a partial called `recipeDetailsPartial` for the 
+  // `description` and `ingredients` of the recipe.
+  // Create a template with an `id` of `recipe-details-partial` to
+  // hold the markup. Use the `each` helper to display the collection
+  // of `ingredients`.
+
+  // Handlebars.registerPartial('ingredientsPartial',
+  // document.getElementsByName('ingredients').innerHTML)
+
 }
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
