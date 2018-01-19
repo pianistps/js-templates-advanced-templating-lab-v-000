3757699c7f6de1e8204a44d6dca1bb196db66368
github username pianistps <pianistps@yahoo.com>
2018-01-19 13:09:58 -0600

    added registration of partial and notes fr functions

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
+function recipeForm() {
+
+
+  // get main and set template
+  // tell submit action what to do (createrecipe)
+}
+
+
+
 function init() {
   //put any page initialization/handlebars initialization here
 
-  // Register a partial called `recipeDetailsPartial` for the 
+  // Register a partial called `recipeDetailsPartial` for the
   // `description` and `ingredients` of the recipe.
   // Create a template with an `id` of `recipe-details-partial` to
   // hold the markup. Use the `each` helper to display the collection
   // of `ingredients`.
+  // Handlebars.registerHelper('displayIngredient', function(ingredients) {
+  //
+  // })
 
-  // Handlebars.registerPartial('ingredientsPartial',
-  // document.getElementsByName('ingredients').innerHTML)
+  Handlebars.registerPartial('recipeDetailsPartial',
+  document.getElementById('recipe-details-partial').innerHTML)
 
+  Handlebars.registerPartial('recipeFormPartial',
+  document.getElementById('recipe-form-partial').innerHTML)
 }
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
+  //init form
 })
